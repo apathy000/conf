b5f910416619e07da0eed3ff7ff60cb8cdf6d7ef
apathy000 <vardanyanv367@gmail.com>
2026-06-25 00:23:24 +0400

    j

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Link, useNavigate } from "react-router-dom";
-import {useEffect, useState} from "react";
 
 
 
