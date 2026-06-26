1a1b415772dd6fc77c181971c15fb44560b64ebc
apathy000 <vardanyanv367@gmail.com>
2026-06-26 18:44:06 +0400

    b

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://slehmlwqdcygdbsdcrjr.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_9FxjJDB1lH7YTjlWUppBHQ_thYYr..."; // paste full key
+const SUPABASE_ANON_KEY = "sb_publishable_9FxjJDB1lH7YTjlWUppBHQ_thYYrdy8"; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
