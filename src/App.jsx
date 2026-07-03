e76b2f64fe8bda141373edd230599935b24dacfd
apathy000 <vardanyanv367@gmail.com>
2026-07-03 16:53:00 +0400

    uty

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import AvatarSetup from "./pages/AvatarSetup";
-import Feed from "./pages/Feed";
-import Register from "./pages/Register";
+import FeedPage from "./pages/FeedPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/avatar" element={<AvatarSetup />} />
-        <Route path="/feed" element={<Feed />} />
+        <Route path="/feed" element={<FeedPage />} />
       </Routes>
     </BrowserRouter>
   );
